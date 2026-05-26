4d2ef1fcc3b022d39cb0eb67de3b57a28122df23
Julio Sergio Domiciano <julio.domiciano@escola.pr.gov.br>
2026-05-25 21:58:37 -0300

    feat dashboard e building com o modal do checklist utilizando dados mocados

--- cofront-web/src/router/index.js
+++ cofront-web/src/router/index.js
@@ -1,11 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Login from '../pages/Login/index.vue'
 import Dashboard from '../pages/Dashboard/index.vue'
+import Buildings from '../pages/Buildings/index.vue'
 
 const routes = [
   { path: '/', redirect: '/login' },
   { path: '/login', component: Login },
   { path: '/dashboard', component: Dashboard },
+  { path: '/buildings/:id', component: Buildings },
 ]
 
 export default createRouter({
