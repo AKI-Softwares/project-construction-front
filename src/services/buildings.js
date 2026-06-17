07f66a62cb322cd4778f4ae8458216b95f3541a2
Julio Sergio Domiciano <julio.domiciano@escola.pr.gov.br>
2026-06-17 07:31:58 -0300

    Update buildings.js

--- src/services/buildings.js
+++ src/services/buildings.js
@@ -1,10 +1,9 @@
 import api from './api.js'
 
-export async function getBuildings() {
-  const response = await api.get('/buildings')
+export async function getBuilding(id) {
+  const response = await api.get(`/buildings/${id}`)
   return response.data
 }
-
 // Adicionando a função que a página está exigindo:
 export async function createBuilding(data) {
   const response = await api.post('/buildings', data)
