1ecc7ee95214daea811e5fa10359e475221b90a4
Julio Sergio Domiciano <julio.domiciano@escola.pr.gov.br>
2026-06-18 18:05:07 -0300

    Update roles.js

--- src/services/roles.js
+++ src/services/roles.js
@@ -1,7 +1,16 @@
-
 import api from './api.js'
 
 export async function getRoles() {
   const response = await api.get('/roles')
+  return response.data
+}
+
+export async function createRole(data) {
+  const response = await api.post('/roles', data)
+  return response.data
+}
+
+export async function deleteRole(id) {
+  const response = await api.delete(`/roles/${id}`)
   return response.data
 }
