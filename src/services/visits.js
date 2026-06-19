84ecf98c7f675325919f2b4e4c25f8fd9fb2815f
Julio Sergio Domiciano <julio.domiciano@escola.pr.gov.br>
2026-06-19 07:59:49 -0300

    Update visits.js

--- src/services/visits.js
+++ src/services/visits.js
@@ -1,1 +1,12 @@
 
+import api from './api.js'
+
+export async function getVisits() {
+  const response = await api.get('/checklists')
+  return response.data
+}
+
+export async function getVisit(id) {
+  const response = await api.get(`/checklists/${id}`)
+  return response.data
+}
