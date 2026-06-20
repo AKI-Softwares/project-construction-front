5fbb032a3ebf89243227f4d6ab98fa9821cdf5e2
Julio Sergio Domiciano <julio.domiciano@escola.pr.gov.br>
2026-06-20 11:52:04 -0300

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -18,6 +18,7 @@ const routes = [
   { path: '/buildings/:id', component: () => import('../pages/Buildings/index.vue'), meta: { requiresAuth: true } },
 
   { path: '/apartment-types', component: () => import('../pages/ApartmentTypes/index.vue'), meta: { requiresAuth: true } },
+  { path: '/services', component: () => import('../pages/Services/index.vue'), meta: { requiresAuth: true } },
 
   { path: '/team/register', component: () => import('../pages/Team/Register.vue'), meta: { requiresAuth: true } },
   { path: '/team', component: () => import('../pages/Team/index.vue'), meta: { requiresAuth: true } },
